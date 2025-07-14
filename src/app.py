e15f40e590a94de28e5a6dc11552495ee53ccb0f
Jack Porter <107512444+jporte111@users.noreply.github.com>
2025-07-14 23:38:09 +0000

    2nd attempt

--- src/app.py
+++ src/app.py
@@ -12,19 +12,31 @@ load_dotenv()
 
 # Spotify API credentials
 client_id = os.environ.get("CLIENT_ID")
-client_secret = os.environ.get("CLIENT_SECRET")
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 spotify = spotipy.Spotify(auth_manager=auth_manager)
 
 artist_id = "6DPYiyq5kWVQS4RGwxzPC7"
-artist_best_tracks = spotify.artist_top_tracks(artist_id)
 
+# Getting the top 3 most popular songs
+results = spotify.artist_top_tracks(artist_id)["tracks"]
 top_songs = []
-for track in artist_best_tracks[track]:
-    top_songs.append ({
-        'name': track[]
-    })
+for track in results:
+    top_songs.append({
+        'name': t['name'],
+        'duration_min': t['duration_ms']['name'],
+        'popularity': t['popularity'],
+        'preview_url': t['preview_url'],})
+
+track_df = pd.dataframe(top_songs)
+
+print(track_df(3))
+
+plt.scatter(track_df['duration_min'], track_df['popularity'])
+plt.xlabel('Duration (Min)')
+plt.ylabel('Popularity')
+plt.title('Comparison of Popularity and Song Length')
+plt.show()
